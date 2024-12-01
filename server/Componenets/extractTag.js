bdbafd5d8b234033d70ed3d4fdea28e3f7f22627
Adeel <adeeltahir6a@gmail.com>
2024-12-01 03:32:16 -0800

    --updated the logic of lemmatizers,forward, and inverted index --

--- server/Componenets/extractTag.js
+++
@@ -1,7 +0,0 @@
-import { JSDOM } from 'jsdom'; 
-
-export default function extractTag(type, content) {
-    const dom = new JSDOM(content);
-    const elements = dom.window.document.getElementsByTagName(type);
-    return Array.from(elements).map(el => el.innerHTML.trim());
-}
